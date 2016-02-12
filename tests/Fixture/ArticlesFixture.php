92b3a123bcc5b0660a519fc65247b6a175a935d4
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2016-02-12 14:00:52 +0100

    Added tests that were not committed before

---
+++ tests/Fixture/ArticlesFixture.php
@@ -0,0 +1,34 @@
+<?php
+namespace RowLocker\Test\Fixture;
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * Short description for class.
+ *
+ */
+class ArticlesFixture extends TestFixture
+{
+    /**
+     * fields property
+     *
+     * @var array
+     */
+    public $fields = [
+        'id' => ['type' => 'integer'],
+        'title' => ['type' => 'string', 'null' => true],
+        'locked_time' => ['type' => 'datetime', 'null' => true],
+        'locked_by' => ['type' => 'string', 'null' => true],
+        'locked_session' => ['type' => 'string', 'null' => true],
+        '_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]]
+    ];
+    /**
+     * records property
+     *
+     * @var array
+     */
+    public $records = [
+        ['title' => 'first'],
+        ['title' => 'second'],
+        ['title' => 'third'],
+    ];
+}
--- tests/TestCase/Model/Behavior/RowLockerBehaviorTest.php
+++ tests/TestCase/Model/Behavior/RowLockerBehaviorTest.php
@@ -3,12 +3,26 @@ namespace RowLocker\Test\TestCase\Model\Behavior;
 
 use Cake\TestSuite\TestCase;
 use RowLocker\Model\Behavior\RowLockerBehavior;
+use RowLocker\LockableInterface;
+use RowLocker\LockableTrait;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+
+class TestEntity extends Entity implements LockableInterface
+{
+    use LockableTrait;
+}
+
 
 /**
  * RowLocker\Model\Behavior\RowLockerBehavior Test Case
  */
 class RowLockerBehaviorTest extends TestCase
 {
+
+    public $fixtures = [
+        'plugin.row_locker.articles'
+    ];
 
     /**
      * setUp method
@@ -18,7 +32,8 @@ class RowLockerBehaviorTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->RowLocker = new RowLockerBehavior();
+        $this->table = TableRegistry::get('Articles', ['entityClass' => TestEntity::class]);
+        $this->table->addBehavior('RowLocker.RowLocker');
     }
 
     /**
@@ -28,18 +43,28 @@ class RowLockerBehaviorTest extends TestCase
      */
     public function tearDown()
     {
-        unset($this->RowLocker);
-
         parent::tearDown();
     }
 
-    /**
-     * Test initial setup
-     *
-     * @return void
-     */
-    public function testInitialization()
+    public function testUnlocked()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $results = $this->table
+            ->find('unlocked', ['lockingUser' => 'lorenzo'])
+            ->toArray();
+        $this->assertCount(3, $results);
+
+        $article = $results[0];
+        $article->lock('someone-else');
+        $this->table->save($article);
+
+        $results = $this->table
+            ->find('unlocked', ['lockingUser' => 'lorenzo'])
+            ->toArray();
+        $this->assertCount(2, $results);
+
+        $results = $this->table
+            ->find('unlocked', ['lockingUser' => 'someone-else'])
+            ->toArray();
+        $this->assertCount(3, $results);
     }
 }
